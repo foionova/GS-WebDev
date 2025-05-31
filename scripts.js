cfed7a30294db351349ebbb341c4cd9279a9a7e9
foionova <rafaelfelixsouza943@gmail.com>
2025-05-31 17:05:43 -0300

    validacao formulario js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 //slide show
-let index = 0;
-const slides = document.querySelectorAll('.slide');
+let index = 0
+const slides = document.querySelectorAll('.slide')
 
 setInterval(() => {
-    slides[index].style.display = 'none';
-    index = (index + 1) % slides.length;
-    slides[index].style.display = 'block';
-}, 3000);
+    slides[index].style.display = 'none'
+    index = (index + 1) % slides.length
+    slides[index].style.display = 'block'
+}, 3000)
+
+//validacao do formulario
+document.getElementById('contatoForm').addEventListener('submit', function(e) {
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+    if (!nome || !email) {
+        alert('Por favor, preencha todos os campos obrigatórios.')
+        e.preventDefault()
+    }
+})
